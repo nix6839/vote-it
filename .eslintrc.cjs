4b0f56e80a3adc6543be240f6ec60afebefa4e66
Han Yeong-woo <han@yeongwoo.dev>
2023-09-20 15:44:23 +0900

    feat: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+const config = {
+  extends: ['next/core-web-vitals', 'prettier'],
+};
+
+module.exports = config;
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+};
+export default config;
