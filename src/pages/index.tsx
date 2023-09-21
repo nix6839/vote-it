dead698d319cfcfe13e5a8978279e08d143cae77
Han Yeong-woo <han@yeongwoo.dev>
2023-09-22 05:20:11 +0900

    feat: header and logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import LogoIcon from '@/components/LogoIcon.tsx';
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>Vote it!</h1>;
+  return (
+    <>
+      <header>
+        <Link href="/" aria-label="메인으로 이동">
+          <LogoIcon width="48" height="48" aria-hidden />
+        </Link>
+      </header>
+    </>
+  );
 }
