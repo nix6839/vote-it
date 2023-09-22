749ee6b7f8a27fbb4b519bc7a5628efc39f2fc91
Han Yeong-woo <han@yeongwoo.dev>
2023-09-23 00:09:46 +0900

    chore: add eslint-plugin-testing-library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,12 @@
 const config = {
   extends: ['next/core-web-vitals', 'prettier'],
+
+  overrides: [
+    {
+      files: ['**/*.test.{ts,tsx}'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 };
 
 module.exports = config;
