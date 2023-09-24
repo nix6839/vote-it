9fb113fc790d42d635c8ad698a267f329d19f32b
Han Yeong-woo <han@yeongwoo.dev>
2023-09-25 07:22:00 +0900

    refactor: use paths alias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import type { AppProps } from 'next/app';
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'true') {
-  await import('../../tests/mocks/isomorphic-init.ts');
+  await import('@tests/mocks/isomorphic-init.ts');
 }
 
 export default function App(props: AppProps) {
