02ae4bae9e5a92201c5d4ff5feeaeb2e0c5c567e
Han Yeong-woo <han@yeongwoo.dev>
2023-10-02 04:37:36 +0900

    feat: update api types and mock data

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,13 +1,26 @@
+interface PollOption {
+  id: number;
+  content: string;
+  votedCount: number;
+  isVoted: boolean;
+}
+
 export interface Poll {
   id: number;
   createdAt: string;
   subject: string;
+  isPlural: boolean;
   expirationDate: string | null;
   picture: string | null;
   participatedCount: number;
   author: Author;
+  options: PollOption[];
+  isVoted: boolean;
 }
 
 export interface Author {
+  id: number;
+  email: string;
   nickname: string;
+  picture: string | null;
 }
