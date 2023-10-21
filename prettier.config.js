c1d8a176c8334291df82d58f6ce946c5a7e027e7
Han Yeong-woo <han@yeongwoo.dev>
2023-10-22 03:25:25 +0900

    chore: apply prettier-plugin-tailwindcss (#10)

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,7 @@ import hwyConfig from '@nix6839/prettier-config';
 /** @satisfies {import('prettier').Config} */
 const config = {
   ...hwyConfig,
+  plugins: ['prettier-plugin-tailwindcss'],
 };
 
 export default config;
